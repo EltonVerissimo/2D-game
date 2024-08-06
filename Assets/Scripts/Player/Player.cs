4d037d6688ac99eea2b582d014f33c17cafae105
Elton Verissimo <55025912+EltonVerissimo@users.noreply.github.com>
2024-08-06 17:39:47 -0300

    add jump to prototype

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,20 +5,42 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D playerRigidbody;
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
+    public float jumpForce = 2;
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        HandleMovement();
+        HandleJump();
+    }
+
+    private void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             //rigidbody.MovePosition(rigidbody.position - velocity * Time.deltaTime);
             playerRigidbody.velocity = new Vector2(-speed, playerRigidbody.velocity.y);
         }
-        else if(Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             //rigidbody.MovePosition(rigidbody.position + velocity * Time.deltaTime);
             playerRigidbody.velocity = new Vector2(speed, playerRigidbody.velocity.y);
         }
+
+        if(playerRigidbody.velocity.x > 0)
+        {
+            playerRigidbody.velocity -= friction;
+        }
+        else if(playerRigidbody.velocity.x < 0)
+        {
+            playerRigidbody.velocity += friction;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+            playerRigidbody.velocity = Vector2.up * jumpForce;
     }
 }
