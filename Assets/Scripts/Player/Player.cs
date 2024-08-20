b5067dce55b948a43bba636817e85d9627c605ed
Elton Verissimo <55025912+EltonVerissimo@users.noreply.github.com>
2024-08-20 18:33:45 -0300

    dgtween de pulo do personagem

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody2D playerRigidbody;
+
+    [Header("Speed setup")]
     public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
     public float speedRun;
@@ -12,15 +15,33 @@ public class Player : MonoBehaviour
 
     private float _currentSpeed;
 
+    [Header("Check ground setup")]
+
+    public float groundCheckDistance = 1.0f;
+    public LayerMask groundLayer;
+
+    private bool isGrounded;
+
+    [Header("Animation setup")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = .7f;
+    public float landScaleY = .7f;
+    public float landpScaleX = 1.5f;
+    public float animationDuration = .3f;
+    public Ease easeOutback = Ease.OutBack;
+
     void Update()
     {
+        if (Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer))
+            handleLandScale();
+
         HandleMovement();
         HandleJump();
     }
 
     private void HandleMovement()
     {
-        _currentSpeed = (Input.GetKey(KeyCode.LeftControl)) ? speedRun: speed;
+        _currentSpeed = (Input.GetKey(KeyCode.LeftControl)) ? speedRun : speed;
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -31,11 +52,11 @@ public class Player : MonoBehaviour
             playerRigidbody.velocity = new Vector2(_currentSpeed, playerRigidbody.velocity.y);
         }
 
-        if(playerRigidbody.velocity.x > 0)
+        if (playerRigidbody.velocity.x > 0)
         {
             playerRigidbody.velocity -= friction;
         }
-        else if(playerRigidbody.velocity.x < 0)
+        else if (playerRigidbody.velocity.x < 0)
         {
             playerRigidbody.velocity += friction;
         }
@@ -43,7 +64,29 @@ public class Player : MonoBehaviour
 
     private void HandleJump()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
-            playerRigidbody.velocity = Vector2.up * jumpForce;
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+            if (isGrounded)
+            {
+                playerRigidbody.velocity = Vector2.up * jumpForce;
+                playerRigidbody.transform.localScale = Vector2.one;
+                DOTween.Kill(playerRigidbody.transform);
+                handleScaleJump();
+            }
+
+        }
+    }
+
+    private void handleScaleJump()
+    {
+        playerRigidbody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(easeOutback);
+        playerRigidbody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(easeOutback);
+    }
+
+    private void handleLandScale()
+    {
+        playerRigidbody.transform.DOScaleY(landScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(easeOutback);
+        playerRigidbody.transform.DOScaleX(landpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(easeOutback);
     }
 }
