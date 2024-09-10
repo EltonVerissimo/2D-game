7e437ed158ba21d75074190aa17ee290be857c9e
Elton Verissimo <55025912+EltonVerissimo@users.noreply.github.com>
2024-09-10 16:11:42 -0300

    New animations (jump, land, sprint)

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -29,32 +29,73 @@ public class Player : MonoBehaviour
     public float jumpScaleX = .7f;
     public float landScaleY = .7f;
     public float landpScaleX = 1.5f;
-    public float animationDuration = .3f;
+    public float animationDuration = .1f;
     public Ease easeOutback = Ease.OutBack;
+    [SerializeField]
+    private bool leftFlip = false;
+
+    [Header("Animation player")]
+    public string boolRun = "Run";
+    public string boolSprint = "Sprint";
+    public Animator animator;
+    public float timeStamp = 0.8f;
+    public float coolDownPeriodInSeconds = 0.8f;
 
     void Update()
     {
         if (Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer) && !isGrounded)
         {
+            if (isGrounded == false)
+                animator.SetTrigger("Land");
             handleLandScale();
             isGrounded = true;
-        }   
+        }
 
+        if (timeStamp <= Time.time)
+        {
+            HandleJump();
+        }
         HandleMovement();
-        HandleJump();
     }
 
     private void HandleMovement()
     {
-        _currentSpeed = (Input.GetKey(KeyCode.LeftControl)) ? speedRun : speed;
+        // _currentSpeed = (Input.GetKey(KeyCode.LeftControl)) ? speedRun : speed;
+
+        if ((Input.GetKey(KeyCode.LeftControl)))
+        {
+            _currentSpeed = speedRun;
+            animator.SetBool(boolSprint, true);
+        }
+        else
+        {
+            _currentSpeed = speed;
+            animator.SetBool(boolSprint, false);
+        }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            leftFlip = true;
             playerRigidbody.velocity = new Vector2(-_currentSpeed, playerRigidbody.velocity.y);
+            if (playerRigidbody.transform.localScale.x != -1)
+            {
+                playerRigidbody.transform.DOScaleX(-1, .1f);
+            }
+            animator.SetBool(boolRun, true);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
+            leftFlip = false;
             playerRigidbody.velocity = new Vector2(_currentSpeed, playerRigidbody.velocity.y);
+            if (playerRigidbody.transform.localScale.x != 1)
+            {
+                playerRigidbody.transform.DOScaleX(1, .1f);
+            }
+            animator.SetBool(boolRun, true);
+        }
+        else
+        {
+            animator.SetBool(boolRun, false);
         }
 
         if (playerRigidbody.velocity.x > 0)
@@ -73,9 +114,11 @@ public class Player : MonoBehaviour
         {
             if (isGrounded)
             {
+                animator.SetTrigger("Jump");
                 isGrounded = false;
+                timeStamp = Time.time + coolDownPeriodInSeconds;
                 playerRigidbody.velocity = Vector2.up * jumpForce;
-                playerRigidbody.transform.localScale = Vector2.one;
+                playerRigidbody.transform.localScale = new Vector2(leftFlip ? -1 : 1, 1);
                 DOTween.Kill(playerRigidbody.transform);
                 handleScaleJump();
             }
@@ -85,13 +128,28 @@ public class Player : MonoBehaviour
 
     private void handleScaleJump()
     {
+        if (leftFlip)
+        {
+            playerRigidbody.transform.DOScaleX(-jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(easeOutback);
+        }
+        else
+        {
+
+            playerRigidbody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(easeOutback);
+        }
         playerRigidbody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(easeOutback);
-        playerRigidbody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(easeOutback);
     }
 
     private void handleLandScale()
     {
+        if(leftFlip)
+        {
+            playerRigidbody.transform.DOScaleX(-landpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(easeOutback);
+        }
+        else
+        {
+            playerRigidbody.transform.DOScaleX(landpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(easeOutback);
+        }
         playerRigidbody.transform.DOScaleY(landScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(easeOutback);
-        playerRigidbody.transform.DOScaleX(landpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(easeOutback);
     }
 }
